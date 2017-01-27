c60ab5d71908178d5c354f52ba570c9eef56ecf5
Kesavan Yogarajah <ykesan@gmail.com>
2017-01-27 16:46:49 +0530

    Update MagentoConnectorIntegrationTest.java

--- src/test/java/org/wso2/carbon/connector/integration/test/magento/MagentoConnectorIntegrationTest.java
+++ src/test/java/org/wso2/carbon/connector/integration/test/magento/MagentoConnectorIntegrationTest.java
@@ -57,7 +57,7 @@ public class MagentoConnectorIntegrationTest extends ConnectorIntegrationTestBas
     @BeforeClass(alwaysRun = true)
     public void setEnvironment() throws Exception {
 
-        init("magento-connector-1.0.1-SNAPSHOT");
+        init("magento-connector-1.0.1");
 
         apiRequestHeadersMap.putAll(esbRequestHeadersMap);
 
